b0eaac66ee2428817bab3eb874c4436cdc0b7aa4
Bravo Ortiz <nabz@gft.com>
2024-11-05 11:58:37 +0100

    Fixing errors in aggregates and controller

--- src/main/java/com/workshop/vehicle/vehicle_service/application/controller/VehicleCommandController.java
+++ src/main/java/com/workshop/vehicle/vehicle_service/application/controller/VehicleCommandController.java
@@ -4,7 +4,8 @@ import com.workshop.vehicle.vehicle_service.application.response.service.Vehicle
 import com.workshop.vehicle.vehicle_service.application.service.VehicleCommandService;
 import com.workshop.vehicle.vehicle_service.domain.model.aggregates.Vehicle;
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -13,10 +14,10 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/vehicles")
 public class VehicleCommandController {
 
+    private static final Logger logger = LoggerFactory.getLogger(VehicleCommandController.class);
     private final VehicleCommandService vehicleCommandService;
     private final VehicleResponseService vehicleResponseService;
 
-    @Autowired
     public VehicleCommandController(VehicleCommandService vehicleCommandService,
                                     VehicleResponseService vehicleResponseService) {
         this.vehicleCommandService = vehicleCommandService;
@@ -25,21 +26,34 @@ public class VehicleCommandController {
 
     @PostMapping
     public Mono<ResponseEntity<Vehicle>> createVehicle(@RequestBody Vehicle vehicle) {
+        logger.info("Creating a new vehicle");
         return vehicleCommandService.createVehicle(vehicle)
-                .flatMap(vehicleResponseService::buildOkResponse);
+                .flatMap(vehicleResponseService::buildOkResponse)
+                .doOnSuccess(response -> logger.info("Successfully created vehicle with ID: {}", response.getBody() != null ? response.getBody().getVehicleId() : "N/A"))
+                .doOnError(error -> logger.error("Error occurred while creating vehicle", error));
     }
 
     @PutMapping("/{idString}")
-    public Mono<ResponseEntity<Vehicle>> updateVehicle(@PathVariable String idString, @RequestBody Vehicle vehicle) {
+    public Mono<ResponseEntity<Vehicle>> updateVehicle(@PathVariable("idString") String idString, @RequestBody Vehicle vehicle) {
         ObjectId id = new ObjectId(idString);
+        logger.info("Updating vehicle with ID: {}", id);
         return vehicleCommandService.updateVehicle(id, vehicle)
-                .flatMap(vehicleResponseService::buildOkResponse);
+                .flatMap(vehicleResponseService::buildOkResponse)
+                .doOnSuccess(response -> logger.info("Successfully updated vehicle with ID: {}", id))
+                .doOnError(error -> logger.error("Error occurred while updating vehicle with ID: {}", id, error))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{idString}")
-    public Mono<ResponseEntity<Void>> deleteVehicle(@PathVariable String idString) {
+    public Mono<ResponseEntity<Void>> deleteVehicle(@PathVariable("idString") String idString) {
         ObjectId id = new ObjectId(idString);
+        logger.info("Deleting vehicle with ID: {}", id);
         return vehicleCommandService.deleteVehicle(id)
-                .then(Mono.fromCallable(() -> ResponseEntity.noContent().build()));
+                .then(Mono.defer(() -> {
+                    logger.info("Successfully deleted vehicle with ID: {}", id);
+                    return Mono.just(ResponseEntity.noContent().<Void>build());
+                }))
+                .doOnError(error -> logger.error("Error occurred while deleting vehicle with ID: {}", id, error));
     }
+
 }
